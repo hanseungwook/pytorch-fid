1c3f5ca20e1f29fdb710110aebc20f13fc8a27e2
Seungwook Han <sh3264@coumbia.edu>
2020-05-15 17:12:52 -0400

    Added shuffle

--- fid_score.py
+++ fid_score.py
@@ -220,7 +220,7 @@ def _compute_statistics_of_path(path, model, batch_size, dims, cuda):
         dataset = f.get('data')
         
         # Shuffle dataset
-        dataset = np.random.shuffle(dataset)
+        dataset = np.random.shuffle(np.array(dataset))
 
         m, s = calculate_activation_statistics(dataset, model, batch_size,
                                                dims, cuda)
